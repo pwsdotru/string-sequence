4f5f4002903b70824db1607b7903fa286f87ab49
Aleksandr Novikov <pwsdotru@gmail.com>
2026-02-22 10:50:32 +0300

    Added internal logic

--- src/Sequencer.php
+++ src/Sequencer.php
@@ -8,6 +8,11 @@ class Sequencer
 {
     private $_sequence;
     private $_length;
+
+    /**
+     * Sequencer constructor.
+     * @param int $length
+     */
     public function __construct(int $length = 0)
     {
         $this->_length = $length;
@@ -17,8 +22,62 @@ class Sequencer
         }
     }
 
+    public function add(string $input): self
+    {
+        $tokens = $this->getTokens($input);
+        array_walk($tokens, [$this, 'addToken']);
+        return $this;
+    }
+
     public function get(): array
     {
         return $this->_sequence;
+    }
+
+    private function getTokens(string $input): array
+    {
+        if ("" === trim($input)) {
+            return [];
+        }
+        $result = explode(',', $input);
+        return array_map(
+            function (string $i): string {
+                return trim($i);
+            },
+            $result
+        );
+    }
+
+    private function getPosition(int $number): int
+    {
+        //If negative number, then calculate for tail
+        if ($number < 0) {
+            $number = $this->_length + $number + 1;
+        }
+        return $number;
+    }
+
+    private function setSeq(int $number): void
+    {
+        if (
+            1 <= $number && $number <= $this->_length
+            && array_key_exists($number, $this->_sequence)
+        ) {
+            $this->_sequence[$number] = true;
+            return;
+        }
+    }
+
+    private function isIntNumeric(string $input): bool
+    {
+        return is_numeric($input) && $input === (string)(int)$input;
+    }
+
+    private function addToken(string $input): void
+    {
+        if ($this->isIntNumeric($input)) {
+            $this->setSeq($this->getPosition((int)$input));
+            return;
+        }
     }
 }
--- tests/SequencerTest.php
+++ tests/SequencerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use StringSequence\Sequencer;
+use ReflectionClass;
 use PHPUnit\Framework\TestCase;
 
 class SequencerTest extends TestCase
@@ -41,6 +42,101 @@ class SequencerTest extends TestCase
                 2,
                 [1 => false, 2 => false]
             ],
+        ];
+    }
+
+    /**
+     * @dataProvider getTokensSet
+     * @param string $input
+     * @param array $expected
+     * @covers \StringSequence\Sequencer::getTokens
+     */
+    public function testGetTokens(string $input, array $expected): void
+    {
+        $obj = new Sequencer();
+        $reflection = new ReflectionClass($obj);
+        $method = $reflection->getMethod('getTokens');
+        $method->setAccessible(true);
+        $result = $method->invoke($obj, $input);
+        self::assertIsArray($result);
+        self::assertEquals($expected, $result);
+    }
+
+    public static function getTokensSet(): array
+    {
+        return [
+            [
+                "1,2,3",
+                ["1", "2", "3"],
+            ],
+            [
+                "",
+                [],
+            ],
+            [
+                "*/2",
+                ["*/2"],
+            ],
+            [
+                "1, 4, */2,4-5",
+                ["1", "4", "*/2", "4-5"],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider isIntNumericSet
+     * @param string $input
+     * @param bool $expected
+     * @covers \StringSequence\Sequencer::isIntNumeric
+     */
+    public function testIsIntNumeric(string $input, bool $expected)
+    {
+        $obj = new Sequencer();
+        $reflection = new ReflectionClass($obj);
+        $method = $reflection->getMethod('isIntNumeric');
+        $method->setAccessible(true);
+        $result = $method->invoke($obj, $input);
+        self::assertSame($expected, $result);
+    }
+
+    public static function isIntNumericSet(): array
+    {
+        return [
+            ["2", true],
+            ["34.5", false],
+            ["345", true],
+            ["-45", true],
+            ["erets", false],
+            ["45w", false],
+            ["56789", true],
+        ];
+    }
+
+    /**
+     * @dataProvider getPositionSet
+     * @param int $input
+     * @param int $expected
+     * @covers \StringSequence\Sequencer::getPosition
+     */
+    public function testGetPosition(int $input, int $expected): void
+    {
+        $obj = new Sequencer(10);
+        $reflection = new ReflectionClass($obj);
+        $method = $reflection->getMethod('getPosition');
+        $method->setAccessible(true);
+        $result = $method->invoke($obj, $input);
+        self::assertSame($expected, $result);
+    }
+
+    public static function getPositionSet(): array
+    {
+        return [
+          [1, 1],
+          [3, 3],
+          [-1, 10],
+          [-4, 7],
+          [0, 0],
         ];
     }
 }
